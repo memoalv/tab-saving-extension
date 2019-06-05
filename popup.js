b4f219f6ddaeabea3b1db73dc78f019ac1a45556
Guillermo Alvarez <gjalvarez09@gmail.com>
2019-06-04 20:23:32 -0600

    otro dia , 2 commits

--- popup.js
+++ popup.js
@@ -1,16 +1,6 @@
-let saveTabs = document.getElementById('btn-saveTabs');
-saveTabs.onclick = getActiveTabs;
+let saveTabs_btn = document.getElementById('btn-saveTabs');
+saveTabs_btn.onclick = getActiveTabs;
 
-window.onload = getUserInfo();
-
-let user = {};
-
-function getUserInfo() {
-    chrome.identity.getProfileUserInfo(userInfo => {
-        user.email = userInfo.email;
-        user.id = userInfo.id;
-    })
-}
 
 /**
  * Prepares an array of active links and sends it to saveActiveTabs
@@ -30,25 +20,46 @@ function getActiveTabs() {
 }
 
 /**
- * Saves an array of links to DB
+ * Saves an array of links to Local storage
  * @param {array} tabsURLs
  */
 function saveActiveTabs(tabsURLs) {
+    console.log(tabsURLs)
+    //TODO: use local storage API to save tabs
+    //https://developer.chrome.com/apps/storage
 
-    let http = new XMLHttpRequest();
+    localStorage.setItem(sessions);
 
-    http.onreadystatechange = function () {
-        if (http.readyState == 4 && http.status != 200) {
-            alert('An error occurred while saving the data.')
+    if (localStorage.getItem(sessions) === null) {
+        // tabs object didn't exist on localStorage
+        let newSessionsArray = [];
+        newSessionsArray.push(getSessionData());
+
+        let sessionsObj = {
+            sessions: newSessionsArray
         }
+
+        localStorage.setItem(JSON.stringify(sessionsObj));s
+    }
+    else {
+        // tabs object exists on localStorage
+        const previousTabs = JSON.parse(localStorage.getItem(sessions));
+        previousTabs.sessions.push(getSessionData(tabsURLs));
+        localStorage.setItem(JSON.stringify(tabsURLs));
     }
 
-    console.log(tabsURLs);
+}
 
-
-    http.open("PUT", `https://save-tabs.firebaseio.com/users/${user.id}/${getMoment()}.json`, true);
-    http.setRequestHeader("Content-Type", "application/json");
-    http.send(JSON.stringify(tabsURLs));
+/**
+ * Return the current session object
+ * @param {array} tabs
+ * Current active tabs  
+ */
+function getSessionData(tabs) {
+    return {
+        timeStamp: getMoment(),
+        tabs: tabs
+    }
 }
 
 /**
